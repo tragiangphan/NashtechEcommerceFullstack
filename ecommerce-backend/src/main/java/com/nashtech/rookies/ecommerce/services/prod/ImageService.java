da7a44e9ff1e0de7070d29a3040677727aeb0534
River-Tea <tragiang.private@gmail.com>
2024-05-18 17:14:24 +0700

    Update getImage service

--- ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/services/prod/ImageService.java
+++ ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/services/prod/ImageService.java
@@ -1,11 +1,18 @@
 package com.nashtech.rookies.ecommerce.services.prod;
 
+import java.util.List;
+
 import com.nashtech.rookies.ecommerce.dto.prod.requests.ImageRequestDTO;
+import com.nashtech.rookies.ecommerce.dto.prod.responses.ImageResponseDTO;
 import com.nashtech.rookies.ecommerce.models.prod.Image;
 import com.nashtech.rookies.ecommerce.services.CommonService;
 
-
 public interface ImageService extends CommonService<Image, Long> {
-  Image createNewImage(ImageRequestDTO imgRequestDTO);
-  Image updateExistImage(Long id, ImageRequestDTO imgRequestDTO);
+  ImageResponseDTO createImage(ImageRequestDTO imgRequestDTO);
+
+  List<ImageResponseDTO> getImages();
+
+  List<ImageResponseDTO> getImages(Long id);
+
+  ImageResponseDTO updateImage(Long id, ImageRequestDTO imgRequestDTO);
 }
--- ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/services/prod/impl/ImageServiceImpl.java
+++ ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/services/prod/impl/ImageServiceImpl.java
@@ -1,10 +1,15 @@
 package com.nashtech.rookies.ecommerce.services.prod.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nashtech.rookies.ecommerce.dto.prod.requests.ImageRequestDTO;
-import com.nashtech.rookies.ecommerce.exceptions.NotFoundException;
+import com.nashtech.rookies.ecommerce.dto.prod.responses.ImageResponseDTO;
+import com.nashtech.rookies.ecommerce.exceptions.ResourceNotFoundException;
+import com.nashtech.rookies.ecommerce.mappers.prod.ImageMapper;
 import com.nashtech.rookies.ecommerce.models.prod.Product;
 import com.nashtech.rookies.ecommerce.models.prod.Image;
 import com.nashtech.rookies.ecommerce.repositories.prod.ImageRepository;
@@ -15,35 +20,52 @@ import com.nashtech.rookies.ecommerce.services.prod.ImageService;
 @Service
 @Transactional(readOnly = true)
 public class ImageServiceImpl extends CommonServiceImpl<Image, Long> implements ImageService {
+  private final ImageRepository imageRepository;
+  private final ImageMapper imageMapper;
+  private final ProductRepository productRepository;
 
-  private final ImageRepository prodImgRepository;
-  private final ProductRepository prodRepository;
-
-  ImageServiceImpl(ImageRepository prodImgRepository, ProductRepository prodRepository) {
-    super(prodImgRepository);
-    this.prodImgRepository = prodImgRepository;
-    this.prodRepository = prodRepository;
+  ImageServiceImpl(ImageRepository imageRepository, ProductRepository productRepository, ImageMapper imageMapper) {
+    super(imageRepository);
+    this.imageRepository = imageRepository;
+    this.imageMapper = imageMapper;
+    this.productRepository = productRepository;
   }
 
   @Transactional
-  public Image createNewImage(ImageRequestDTO imgRequestDTO) {
-    Image productImage = new Image();
-    Product product = prodRepository.findById(imgRequestDTO.productId()).orElseThrow(NotFoundException::new);
-    productImage.setImageLink(imgRequestDTO.imageLink());
-    productImage.setImageDesc(imgRequestDTO.imageDesc());
-    productImage.setProduct(product);
-    prodImgRepository.saveAndFlush(productImage);
-    return productImage;
+  public ImageResponseDTO createImage(ImageRequestDTO imgRequestDTO) {
+    Image image = new Image();
+    Product product = productRepository.findById(imgRequestDTO.productId()).orElseThrow(ResourceNotFoundException::new);
+    image.setImageLink(imgRequestDTO.imageLink());
+    image.setImageDesc(imgRequestDTO.imageDesc());
+    image.setProduct(product);
+    image = imageRepository.saveAndFlush(image);
+    return imageMapper.toResponseDTO(image);
+  }
+
+  @Override
+  public List<ImageResponseDTO> getImages() {
+    var images = imageRepository.findAll();
+    List<ImageResponseDTO> imageResponseDTOs = new ArrayList<>();
+    images.forEach(image -> imageResponseDTOs.add(imageMapper.toResponseDTO(image)));
+    return imageResponseDTOs;
+  }
+
+  @Override
+  public List<ImageResponseDTO> getImages(Long id) {
+    var image = imageRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+    List<ImageResponseDTO> imageResponseDTOs = new ArrayList<>();
+    imageResponseDTOs.add(imageMapper.toResponseDTO(image));
+    return imageResponseDTOs;
   }
 
   @Transactional
-  public Image updateExistImage(Long id, ImageRequestDTO imgRequestDTO) {
-    Image productImage = prodImgRepository.findById(id).orElseThrow(NotFoundException::new);
-    Product product = prodRepository.findById(imgRequestDTO.productId()).orElseThrow(NotFoundException::new);
-    productImage.setImageLink(imgRequestDTO.imageLink());
-    productImage.setImageDesc(imgRequestDTO.imageDesc());
-    productImage.setProduct(product);
-    prodImgRepository.saveAndFlush(productImage);
-    return productImage;
+  public ImageResponseDTO updateImage(Long id, ImageRequestDTO imgRequestDTO) {
+    Image image = imageRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+    Product product = productRepository.findById(imgRequestDTO.productId()).orElseThrow(ResourceNotFoundException::new);
+    image.setImageLink(imgRequestDTO.imageLink());
+    image.setImageDesc(imgRequestDTO.imageDesc());
+    image.setProduct(product);
+    imageRepository.saveAndFlush(image);
+    return imageMapper.toResponseDTO(image);
   }
 }
