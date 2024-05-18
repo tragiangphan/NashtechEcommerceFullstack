a050e71a2c3b027caf7c01c262ecb783cd7eb740
River-Tea <tragiang.private@gmail.com>
2024-05-18 16:42:35 +0700

    Config GET method, changed URL of Category RestAPI, bring the processing logic of pushing / receiving data from the Controller to the Service

--- ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/controllers/prod/ImageController.java
+++ ecommerce-backend/src/main/java/com/nashtech/rookies/ecommerce/controllers/prod/ImageController.java
@@ -1,12 +1,10 @@
 package com.nashtech.rookies.ecommerce.controllers.prod;
 
-import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nashtech.rookies.ecommerce.controllers.RestVersion;
 import com.nashtech.rookies.ecommerce.dto.prod.requests.ImageRequestDTO;
 import com.nashtech.rookies.ecommerce.dto.prod.responses.ImageResponseDTO;
+import com.nashtech.rookies.ecommerce.exceptions.ResourceNotFoundException;
 import com.nashtech.rookies.ecommerce.mappers.prod.ImageMapper;
 import com.nashtech.rookies.ecommerce.models.prod.Image;
 import com.nashtech.rookies.ecommerce.services.prod.ImageService;
@@ -32,34 +31,30 @@ public class ImageController extends RestVersion {
     this.imageMapper = imageMapper;
   }
 
-  @PostMapping("/prodImages")
-  public ResponseEntity<Void> createProdImage(@RequestBody @Valid ImageRequestDTO prodImgRequestDTO) {
-    Image prodImage = imageService.createNewImage(prodImgRequestDTO);
-    return ResponseEntity.created(URI.create("/api/v1/prodImages/" + prodImage.getId())).build();
+  @PostMapping("/images")
+  public ResponseEntity<ImageResponseDTO> createProdImage(@RequestBody @Valid ImageRequestDTO prodImgRequestDTO) {
+    return ResponseEntity.ok(imageService.createImage(prodImgRequestDTO));
   }
 
-  @GetMapping("/prodImages")
-  public ResponseEntity<List<ImageResponseDTO>> getAllProdImages() {
-    var images = imageService.findAll();
+  @GetMapping("/images")
+  public ResponseEntity<List<ImageResponseDTO>> getImage(@RequestParam(name = "id", required = false) Long id) {
     var imageResponseDTO = new LinkedList<ImageResponseDTO>();
-    for (var image : images) {
-      ImageResponseDTO prodImageDTO = imageMapper.toResponseDTO(image);
-      imageResponseDTO.add(prodImageDTO);
+    if (id != null) {
+      Image productImage = imageService.findOne(id).orElseThrow(ResourceNotFoundException::new);
+      imageResponseDTO.add(imageMapper.toResponseDTO(productImage));
+    } else {
+      var images = imageService.findAll();
+      for (var image : images) {
+        ImageResponseDTO prodImageDTO = imageMapper.toResponseDTO(image);
+        imageResponseDTO.add(prodImageDTO);
+      }
     }
     return ResponseEntity.ok(imageResponseDTO);
   }
 
-  @GetMapping("/prodImages/{id}")
-  public ResponseEntity<ImageResponseDTO> getProImageById(@RequestParam("id") @PathVariable("id") Long id) {
-    Image productImage = imageService.findOne(id).orElseThrow(IllegalArgumentException::new);
-    ImageResponseDTO prodImgResponseDTO = imageMapper.toResponseDTO(productImage);
-    return ResponseEntity.ok(prodImgResponseDTO);
-  }
-
-  @PutMapping("/prodImages/{id}")
-  public ResponseEntity<ImageResponseDTO> updateProdImageById(@RequestParam("id") @PathVariable Long id,
+  @PutMapping("/images")
+  public ResponseEntity<ImageResponseDTO> updateProdImageById(@RequestParam(name = "id", required = true) Long id,
       @RequestBody ImageRequestDTO imageRequestDTO) {
-    Image productImage = imageService.updateExistImage(id, imageRequestDTO);
-    return ResponseEntity.ok(imageMapper.toResponseDTO(productImage));
+    return ResponseEntity.ok(imageService.updateImage(id, imageRequestDTO));
   }
 }
